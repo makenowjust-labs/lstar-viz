4b46d58c6aa580583904982db92c03c9fd422d42
TSUYUSATO Kitsune <make.just.on@gmail.com>
2025-01-01 14:18:02 +0900

    Introduce shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Angluin's L* Algorithm Visualization
-    </h1>
+    <>
+      <header className="block h-11 border-b border-gray-200 mt-4 mb-4">
+        <h1 className="text-3xl font-bold ml-4 text-primary">
+          Angluin's L* Algorithm Visualization
+          <small className="ml-2 font-normal">
+            made by{" "}
+            <a
+              className="font-medium underline underline-offset-4"
+              href="https://github.com/makenowjust"
+            >
+              @makenowjust
+            </a>
+          </small>
+        </h1>
+      </header>
+    </>
   );
 }
 
